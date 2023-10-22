b7008ddddc93022b181a8e7e81ec8d978a542ba8
root <admin@example.com>
2023-10-22 15:27:30 +0630

    http force

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,23 +3,33 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\Schema as FacadesSchema;
 
 class AppServiceProvider extends ServiceProvider
 {
     /**
      * Register any application services.
+     *
+     * @return void
      */
-    public function register(): void
+    public function register()
     {
-        //
+        FacadesSchema::defaultStringLength(191);
     }
-
     /**
      * Bootstrap any application services.
+     *
+     * @return void
      */
-    public function boot(): void
+    public function boot()
     {
         Paginator::useBootstrap();
+        $env = "local";
+        if (@$_SERVER["APP_ENV"]) $env = $_SERVER["APP_ENV"];
+        if ($env === 'production') {
+            URL::forceScheme('http');
+        }
     }
 }
